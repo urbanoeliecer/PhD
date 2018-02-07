d2ec08ce0450a6f9761f9077a0640206d2170768
Urbano Gómez P <urbano.gomez@upb.edu.co>
2018-02-07 16:55:55 -0500

    Preguntas - BD

--- index.php
+++ index.php
@@ -1,7 +1,47 @@
 <?php
+//Ejemplo de página para el seguimiento del doctorado
 include "0cabecera.php";
 include "0cabezote.php";
 //include "0descripcion.html";
 include "0download.php";
 include "pie.html";
+
+contexto del problema
+
+Conocer el volumen de información
+errores en la toma de decisiones
+perdida de dinero así el animal esté en crecimiento
+
+
+lectura de fincas
+
+Un grupo estario es
+la mejor raza para leche en el juego es
+existen cuatso compoenentes principales
+
+samii-Modelo
+Cual acción es la que inicia el proceso? recibir el dinero, comprar animales, ...
+
+
+
+samii-papel
+Cuánto costó la inseminación, tenga en cuenta que debe quitar el costo de la comida y el agua? 100.000
+a los cuantos meses se le dió por primera vez 30 kg de pasto al animal? 23
+En que edad en meses se destetó el animal por primera vez?  35
+En cuánto se vendió el primer animal destetado? 420.000
+
+
+samii-VS_DSS
+
+Con cuántos animales jugo el aprendiz? 2
+Cuál es el id de la partda? ssqd94
+en que més se enfermó un animal por primera vez? 2
+Al iniciar el mes 4 cuanto dinero tenia el jugador? 3088000 3224000
+cuál es el # de la partida en el resumen de usabilidad? 173
+cuál es el peso del animal 420057 en abril de 2018? 212
+Según el análisis gráfico de informe xx, el animal observado se comportó de acuerdo a lo esperado o estuvo por debajo? por encima, por debajo, igual
+
+
+Una pagina web que permite al usuario sleccionar el proyecto, el portal deberá mostrar el archivo que continua y el usuario deberá abrirlo o descargardo , obteniendo la bandera para que se le habilite otro, de esta forma podrá recorrerlos todos, hasta llegar a la opción de descargar el dss, la gamificación aparece por que con cada archivo que pase irá obteniendo un puntaje
+
 ?>
