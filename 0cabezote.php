a678e4f9249c6fe51695a4159894d7ad74d77d3f
urbanoeliecer <31316981+urbanoeliecer@users.noreply.github.com>
2018-06-08 15:27:04 -0500

    Cambios de Seguridad en Sami

--- 0cabezote.php
+++ 0cabezote.php
@@ -1,12 +1,12 @@
 <div class="container">
-<h1>SARA - Videojuego serio para el aprendizaje agrícola</h1>
+<h1>SAMI - Videojuego serio para el aprendizaje de ganadería</h1>
 </div>  
 <div class="container">
 <div class="row">
 <div class="col-md-3">
-Videojuego de sistemas de producción agrícola, buscando un proceso óptimo con el manejo de recursos.
-<!--<div align="center"><img src="img/logoUPB.png" class="img-responsive"></div>-->
-<br><b>Elaborado por:</b><br>Intelec - UPB B/manga<br>Urbano Eliécer Gómez Prada<br>
+Videojuego de sistemas de producción bovina para el entretenimiento y el aprendizaje que brinda la posibilidad de contraste de las jugadas con los resultados que podrían llegar a obtenerse si se sigue un proceso óptimo con el manejo de recursos.
+<div align="center"><img src="img/logoUPB.png" class="img-responsive"></div>
+<b>Elaborado por:</b><br>Intelec - UPB B/manga<br>Urbano Eliécer Gómez Prada<br>Oscar Fernando Gómez Sandoval<br>
 
 <form class="form-signin" method="POST" action="LogIn.php">
 <?php
@@ -14,13 +14,14 @@ if (isset($_SESSION["usuario"])){
 // ir a cabecera
 }
 else {
-    ?>
+    ?><!--
     <h3>Iniciar Sesión:</h3>
     <table>
     <tr><td>Usuario:<td><input type="text" placeholder="Usuario" name="usuario" size="8" required autofocus>
     <tr><td>Password:<td><input type="password"  name="password" size="8"  placeholder="Contraseña" required>
     <tr><td colspan="2"><input type="submit" value="Enviar" class="btn btn-primary" >
     </table>
+	-->
 <?php
 }
 ?> 
---
+++ 0download.php
@@ -0,0 +1,28 @@
+﻿<div class="col-md-3"><?php
+if (isset($_SESSION["usuario"])){
+    echo 'Hola <font color="red">'.$_SESSION["usuario"].'</font>';
+    ?>
+    <h3>Descargar</h3><a href="/sami/files/Sami.zip">
+    <img src="img/descarga.png" class="img-responsive" width="30"></a>
+    <p>Debe dar clic en la imagen</p>
+    <?php
+}
+ else {
+?><!--<h3>Datos para la Descarga:</h3>
+<form action ="0registroIn.php" method="POST">
+Ingrese los datos y empiece a practicar habilidades administrando ganaderías<br>
+<table>
+<tr><td>Usuario:<td><input name="txtUsr" id="txtUsr">
+<tr><td>Nombre:<td><input name="txtNmb" id="txtNmb">
+<tr><td>Apellido:<td><input name="txtApl" id="txtApl">
+<tr><td>Municipio:<td><input name="txtMnc" id="txtMnc">
+<tr><td>Email:<td><input id= "email" type="text" name="txtCrr">
+<tr><td>Documento:<td><input id= "txtDcm" type="text" name="txtDcm">
+</table><br>
+
+<input  type = "submit" class="btn btn-primary" value="Continuar" onclick="return validar()">
+</form>-->
+<?php
+}
+?>    
+</div>    
