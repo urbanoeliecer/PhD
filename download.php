6951e6ff0afd1e775ed7f5ef52b8341c20f25d9f
urbanoeliecer <31316981+urbanoeliecer@users.noreply.github.com>
2018-06-08 15:28:51 -0500

    Cambios de Seguiridad - Otros

---
+++ download.php
@@ -0,0 +1,24 @@
+<?php
+include "0cabecera.php";
+?>
+
+<div class="col-md-3"><?php
+/*
+session_start();
+if (isset($_SESSION["robots"])){
+    echo 'Hola <font color="red">'.$_SESSION["robots"].'</font>';
+    ?>
+    <h3>Descargar</h3>
+    <img src="imagen.png" class="img-responsive">
+    <p>Has clic en la imagen</p>
+    <?php
+}
+ else {
+    echo 'Debe registrarse para descargar';
+}
+*/
+?>    
+</div>    
+<?php
+include "pie.html"
+?>
--- index.php
+++ index.php
@@ -1,47 +1,18 @@
 <?php
-//Ejemplo de página para el seguimiento del doctorado
-include "0cabecera.php";
-include "0cabezote.php";
-//include "0descripcion.html";
-include "0download.php";
-include "pie.html";
-
-contexto del problema
-
-Conocer el volumen de información
-errores en la toma de decisiones
-perdida de dinero así el animal esté en crecimiento
-
-
-lectura de fincas
-
-Un grupo estario es
-la mejor raza para leche en el juego es
-existen cuatso compoenentes principales
-
-samii-Modelo
-Cual acción es la que inicia el proceso? recibir el dinero, comprar animales, ...
-
-
-
-samii-papel
-Cuánto costó la inseminación, tenga en cuenta que debe quitar el costo de la comida y el agua? 100.000
-a los cuantos meses se le dió por primera vez 30 kg de pasto al animal? 23
-En que edad en meses se destetó el animal por primera vez?  35
-En cuánto se vendió el primer animal destetado? 420.000
-
-
-samii-VS_DSS
-
-Con cuántos animales jugo el aprendiz? 2
-Cuál es el id de la partda? ssqd94
-en que més se enfermó un animal por primera vez? 2
-Al iniciar el mes 4 cuanto dinero tenia el jugador? 3088000 3224000
-cuál es el # de la partida en el resumen de usabilidad? 173
-cuál es el peso del animal 420057 en abril de 2018? 212
-Según el análisis gráfico de informe xx, el animal observado se comportó de acuerdo a lo esperado o estuvo por debajo? por encima, por debajo, igual
-
-
-Una pagina web que permite al usuario sleccionar el proyecto, el portal deberá mostrar el archivo que continua y el usuario deberá abrirlo o descargardo , obteniendo la bandera para que se le habilite otro, de esta forma podrá recorrerlos todos, hasta llegar a la opción de descargar el dss, la gamificación aparece por que con cada archivo que pase irá obteniendo un puntaje
-
+include "cabecera.html"
+?>
+<div class="col-md-3"><!--
+<h3>Datos para la Descarga:</h3>
+<form action ="login.php" method="POST">
+<table ><tr><td>Nombre:<td><input name="txtNmb" id="txtNmb">
+<tr><td>&nbsp;<tr><td>Apellido:<td><input name="txtApl" id="txtApl">
+<tr><td>&nbsp;<tr><td>Municipio:<td><input name="txtMnc" id="txtMnc">
+<tr><td>&nbsp;<tr><td>Email:<td><input id= "email" type="text" name="txtCrr">
+</table><br>
+Ingrese cinco datos y empiece a practicar sus habilidades para programar Robots seguidores de l&iacute;nea.<br>
+<input  type = "submit" class="btn btn-primary" value="Continuar" onclick="return validar()">
+</form>-->
+</div>
+<?php
+include "pie.html"
 ?>
