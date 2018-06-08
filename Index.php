9bec144b9ad38fb1d9456cecac418ff1470c6189
urbanoeliecer <31316981+urbanoeliecer@users.noreply.github.com>
2018-06-08 15:29:24 -0500

    Cambios de Seguridad - Raíz

--- Index.php
+++ Index.php
@@ -1,23 +1,31 @@
-<?php
+﻿<?php
 $root = realpath($_SERVER["DOCUMENT_ROOT"]);
-$AppName = "samii/";
+$AppName = ""; //appgro//
 $SystemFolder = $AppName."sistema";
-//include "$root//$SystemFolder//funciones//Vista.php";
+include "$root//$SystemFolder//funciones//Vista.php";
 include "$root//$SystemFolder//funciones//Menu.php";
-//session_start();
-//if (!isset($_SESSION["usuario"])) {
-//    header("Location: ../IniciarSesion.php");
-//}
+$nmbGlb = 'Intelect';
 ?>
 <!DOCTYPE html>
 <html lang="es">
 <head>
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<script>
+  (adsbygoogle = window.adsbygoogle || []).push({
+    google_ad_client: "ca-pub-5837552012813784",
+    enable_page_level_ads: true
+  });
+</script>
+<meta name="description" content="Ejercicios de Programación. Lógica." />
+<meta name="keywords" content="Fundamentos, Algoritmo, Problemas, Programacion"/>
+<meta name="author" content="urbanoeliecer@gmail.com">
 <meta charset="utf-8">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8_spanish_ci" />
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta name="description" content="">
 <meta name="author" content="">
-<title>SAMII-DSS</title>
+<title><?php echo $nmbGlb;?></title>
 <meta charset="utf-8">
 
 <link rel="stylesheet" href="css/reset.css" type="text/css" media="all">
@@ -32,33 +40,30 @@ include "$root//$SystemFolder//funciones//Menu.php";
 <script src="../sistema/estilos/js/bootstrap.min.js"></script>
 <?php
 //
-$intro = '<b>SAMII-DSS</b> es un sistema de información para apoyar la toma de decisiones en la agroindustria. SAMII-DSS surge como estrategia del trabajo de investigación doctoral “Estrategia metodológica para la apropiación de sistemas de información para el apoyo en la toma de decisiones, soportada en Dinámica de Sistemas y videojuegos. Caso de estudio: Administración de Sistemas Productivos Agroindustriales Bovinos” que se lleva a cabo en la <b>UIB</b>.<br><br><b>SAMII-DSS</b> es su primer producto y permite la administración de ganaderías bovinas en modalidad de Cría, Ceba, Doble Propósito, Lechería o Ciclo Completo. Puede manejar y controlar: Inventario de animales, reproducción, producción de leche, carne, sanidad, alimentación, genealogía, lotes, entre otros, conozca nuestra política de protección de datos <a href="politica.php">aquí</a>. Para conocer mas del videojuego de clic <a href="Videojuego.php">aquí</a>.<br>';
-$tit1 = "Informes";
-$tit2 = ""; //<div class="wrapper"><a href="#" class="link1"><span><span>More Services</span></span></a></div>
-$tit3 = "SAMII-DSS - Novedades"; 
-$tit4 = "SAMII-DSS - Instrucciones";
-$tit5 = "SAMII-DSS";
-$car2 = "";
-$car3 = "Como posibilidad de mejora, SAMII-DSS ofrece la posibilidad de contar con modelos de simulación para contrastar sus resultados con lo de las mejores decisiones que pudo tomar. "; // e identificación electrónica con lectores RFID";
+$intro = '<b>Intelec: Grupo de Inv. en Informática y Telecomunicaciones</b><br><br><img src="imagenes/logoUPB.png"><br><br>El grupo INTELEC tiene por misión generar aportes al conocmiento mundial mediante el desarrollo de proyectos de investigación de alto impacto tecnológico, social y organizacional enfocado en  las Tecnologías de la Información y Telecomunicaciones aportanto a su vez a la formación de Investigadores mediantes proyectos de pregrado, Maestría y Doctorado.<br><br><b>Líneas de Investigación:</b><br>1. Telemática<br>2. Ciberseguridad<br>3. Ingeniería de Software<br>4. Gestión de TI<br>';
+$car1 = " Integrantes";
+$car2 = " Productos";
 ?>
-    </head>
-    <body>
+</head>
+<body>
+
         <!-- Fixed navbar -->
         <nav class="navbar navbar-default navbar-fixed-top">
             <div class="container">
                 <div class="navbar-header">
-                    <!--<img src = "../images/appisSin.png" width="63">-->
-					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
                         <span class="sr-only">Toggle navigation</span>
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
                     </button>
-                    <a class="navbar-brand" href="#">SAMII-DSS</a>
+                    <a class="navbar-brand" href="#">Intelec</a>
                 </div>
                 <div id="navbar" class="navbar-collapse collapse">
-                    <ul class="nav navbar-nav"><?php
-                        echo MenuBas();
+                    <ul class="nav navbar-nav">
+                        <?php
+                        echo '<img src = "imagenes/logo.png" width="47">&nbsp;';
+                        //echo MenuBas();
                         ?>
                     </ul>
                 </div><!--/.nav-collapse -->
@@ -68,7 +73,6 @@ $car3 = "Como posibilidad de mejora, SAMII-DSS ofrece la posibilidad de contar c
 <div id="main">
     <div class="container">
         <section id="content">
-            <div id="slogan"><img src="images/slogan.png"></div>
             <div class="inside">
                 <h2><span>Bienvenido!</span></h2>
                 <p><?php echo $intro; ?></p>
@@ -76,43 +80,69 @@ $car3 = "Como posibilidad de mejora, SAMII-DSS ofrece la posibilidad de contar c
         </section>
         <aside>
             <div class="inside">
-                <ul class="insurance"><?php
-                //echo '<li><strong>'.$tit1.'</strong>'.$car1.'</li>';
-                echo '<li><strong>'.$tit2.'</strong>'.$car2.'</li>';
-                echo '<li><strong>'.$tit3.'</strong>'.$car3.'</li>';
-                echo '<li><strong>'.$tit4.'</strong>';?>
-                <!--<a href = "Manualf.php?id=3"><img src="images/descargablanca.png" width="14">&nbsp;Regístrese</a>-->
-                <!--<br><a href = "Manualf.php?id=2"><img src="../images/descargablanca.png" width="14">&nbsp;Inicie el uso de Appis desde el Celular</a>-->
-                <br><a href = "Manualf.php"><img src="images/descargablanca.png" width="14">&nbsp;Consultar mas información</a>
-				<br><a href="SAMII.zip" download="SAMII-VS_PC"><img src="img/pc.png" width="14">&nbsp;SAMII-VS para PC Windows</a>
-				<br><a href="SAMII.apk" download="SAMII-VS_Cel"><img src="img/android.png" width="14">&nbsp;SAMII-VS para Android</a>
-
-				<?php
-                echo '</li>';
-                echo '<li><strong>'.$tit5.'</strong>';
-				echo 'La estrategia espera que haya Apropiación del DSS a partir de la comprensión de un modelo de simulación desarrollado con Dinámica de Sistemas (DS) que se transformó en un videojuego serio (VS) que genera información que se almacena en el DSS, jugar implica tomar conciencia para que decidadn registrar la información de sus sistemas real. Hay comunidades que haran uso de la metodología y hay una de control que no seguirá la estrategia y solo adoptará la herramienta.';
-               echo'<img src = "img/SamiiApropiacion.png">';
-				//echo '<iframe width="360" height="265" src="https://www.youtube.com/embed/hUippuPj-Ec" frameborder="0" allowfullscreen></iframe>';
+                <ul class="insurance"><table width="100%"><?php
+                echo '<li><strong>'.$nmbGlb.' -'.$car1.'</strong></li>';
+                // Deberá buscar en la base de datos
+                include("0conect.php");
+                $link = Conectarse();
+                $sql = "Select Nombre, Apellido, Email, cvLac from usuarios order by Nombre, Apellido ";
+                mysqli_query($link,"SET NAMES 'UTF8'");    
+                $p = mysqli_query($link,$sql);
+                $j=1; 
+                while ($f = mysqli_fetch_array($p)) {
+                     echo '<tr><td>'.$j.'&nbsp;';
+                    for ($i=0;$i<3;$i++)
+                      echo '<td>'.$f[$i];
+                    echo '<td>&nbsp;';
+                    if ($f[3] != '')
+                      echo '<a href="https://'.$f[3].'"><img src="imagenes/ver.png" width="20"></a>';
+                    $j++;
+                }
                 ?>
+                </table><br><table width="100%"><?php
+                echo '<li><strong>'.$nmbGlb.' -'.$car2.'</strong></li>';
+                $sql = "Select NombreCorto, NombreLargo, anio, url, Tipo, estado from productos where estado = 2 order by Tipo, NombreCorto, Anio ";
+                $p = mysqli_query($link,$sql);
+                $j=1; $cmp='';
+                while ($f = mysqli_fetch_array($p)) {
+                    if ($f[4] != $cmp){
+                        switch($f[4]){
+                            case 1: $xx = 'Proyectos de Investigación'; break;
+                            case 2: $xx = 'Artículos'; break;
+                            case 3: $xx = 'Ponencias'; break;
+                            case 4: $xx = 'Proyectos de Postgrado'; break;
+                            case 5: $xx = 'Proyectos de Grado'; break;
+							case 6: $xx = 'Semilleros'; break;
+                        }
+                        echo '<tr><td colspan="4"><b>'.$xx.'</b>';
+                    }
+                    echo '<tr><td>'.$j;
+                    echo '<td>&nbsp;';
+                    if ($f[5] == 2) echo '<a href="/'.$f[0].'"><img src="imagenes/'.$f[0].'.png" width="25"></a>';
+                    echo '<td><a href="/'.$f[0].'">'.$f[0].'</a>';
+                    echo '<td>'.$f[1];
+                    echo '<td>'.$f[2];
+                    echo '<td>&nbsp;';
+                    if ($f[3] != '')
+                      echo '<a href="https://'.$f[3].'"><img src="imagenes/ver.png" width="20"></a>';
+                    $cmp = $f[4];
+                    $j++;
+                }
+                ?>
+                </table>
                 </ul>
-                
             </div>
         </aside>
     </div>
-<footer>
-    <div align="center">
-        Desarrollado por Urbano E. Gómez Prada (Docente de Ingeniería de Sistemas e Informática de la UPB)
-        <br>Bucaramanga - Colombia<br>
-		<br>2017 - 2018<br>
-		<img src = "img/logosU.png">
-    </div>
-</footer>
-<div align="center">
-<iframe width="340" height="215" src="https://www.youtube.com/embed/8uHL3JFFsD8" frameborder="0" allowfullscreen></iframe><br><br>
-<iframe width="340" height="215" src="https://www.youtube.com/embed/gd1kXhPeqo4" frameborder="0" allowfullscreen></iframe><br><br>
-<iframe width="340" height="215" src="https://www.youtube.com/embed/RhO4-NAFUxQ" frameborder="0" allowfullscreen></iframe>
 
-</div>
-</div>
+    </div>
+<div align="center"> <tr>
+<td> <div style="width: 300px; height: 250px; float: left;">
+
+</div> </td>
+<td><div style="width: 300px; height: 250px; float: right;">
+
+</div></td></tr> 
+</center>	
 </body>
 </html>
